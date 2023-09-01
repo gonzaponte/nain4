623166af8a9d3e738aa4f0e2ca11d742bd8d3caf
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-09-01 15:03:14 +0300

    WIP

--- nain4/src/n4-messenger.hh
+++ nain4/src/n4-messenger.hh
@@ -3,11 +3,30 @@
 namespace nain4 {
   using G4CMD = G4GenericMessenger::G4GenericMessenger::Command;
 
-
   struct command {
-    command(G4CMD& handle_) : handle(handle_) {}
+    G4CMD*   handle;
+    G4String name(){ return handle -> GetCommandName(); }
 
-    #define FORWARD(NEW_NAME, TYPE, OLD_NAME) command& NEW_NAME(TYPE v){handle.OLD_NAME(v); return *this;}
+    command(G4CMD* handle, G4String description="") : handle(handle), description_(description) {}
+
+#define VAR_AND_SETTER(NAME)                                 \
+    std::optional<G4String> NAME##_ = std::nullopt;          \
+    command& NAME(G4String x) { NAME##_ = x; return *this; }
+
+    VAR_AND_SETTER(  dimension)
+    VAR_AND_SETTER(       unit)
+    VAR_AND_SETTER(description)
+    VAR_AND_SETTER(      range)
+    VAR_AND_SETTER(    options)
+    VAR_AND_SETTER(defaults_to)
+
+    G4bool required_;
+    command& required(){required_ =  true; return *this;}
+    command& optional(){required_ = false; return *this;}
+  }
+
+
+#define FORWARD(NEW_NAME, TYPE, OLD_NAME) command& NEW_NAME(TYPE v){handle.OLD_NAME(v); return *this;}
     FORWARD(  dimension, G4String, SetUnitCategory)
     FORWARD(       unit, G4String, SetUnit)
     FORWARD(description, G4String, SetGuidance)
@@ -15,8 +34,6 @@ namespace nain4 {
     FORWARD(    options, G4String, SetCandidates)
     FORWARD( default_to, G4String, SetDefaultValue)
 
-    command& required(){handle.SetParameterName(get_name(),  true); return *this;}
-    command& optional(){handle.SetParameterName(get_name(), false); return *this;}
     #undef FORWARD
 
   private:
