b10a7ea673278c6a3e719389408d1ab9a3919b69
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-09-07 00:43:20 +0300

    Redesign

--- nain4/src/n4-messenger.hh
+++ nain4/src/n4-messenger.hh
@@ -7,20 +7,26 @@
 #include <optional>
 #include <memory>
 
-namespace nain4 {
-#define G4CMD G4GenericMessenger::G4GenericMessenger::Command
 
+namespace nain4 {
+
+#define shared_msg std::shared_ptr<G4GenericMessenger>
+
+#define VAR_AND_SETTER(NAME, TYPE)                              \
+  std::optional<TYPE> NAME##_ = std::nullopt;                   \
+  cmd_config& NAME(TYPE x) { NAME##_ = x; return *this; }
+
+  template<class T>
   struct cmd_config {
   private:
     friend struct messenger;
 
-    G4CMD& handle;
+    shared_msg msg;
     G4String name;
-    cmd_config(G4CMD& handle, G4String name) : handle(handle), name(name) {}
+    T& var;
 
-#define VAR_AND_SETTER(NAME, TYPE)                          \
-    std::optional<TYPE> NAME##_ = std::nullopt;             \
-    cmd_config& NAME(TYPE x) { NAME##_ = x; return *this; }
+    cmd_config(shared_msg msg,  G4String& name, T& var) : msg(msg), name(name), var(var) {}
+
 
   public:
     VAR_AND_SETTER(  dimension, G4String)
@@ -29,11 +35,13 @@ namespace nain4 {
     VAR_AND_SETTER(      range, G4String)
     VAR_AND_SETTER(    options, G4String)
     VAR_AND_SETTER(defaults_to, G4String)
-    VAR_AND_SETTER(   required, G4bool  )
 
+    std::optional<G4bool> required_;
+    cmd_config& required(){required_ =  true; return *this;}
     cmd_config& optional(){required_ = false; return *this;}
 
     void done() {
+      auto handle = msg -> DeclareProperty(name, var);
       if (  dimension_.has_value()) { handle.SetUnitCategory (  dimension_.value()); }
       if (       unit_.has_value()) { handle.SetUnit         (       unit_.value()); }
       if (      range_.has_value()) { handle.SetRange        (      range_.value()); }
@@ -47,7 +55,7 @@ namespace nain4 {
                   << "`.description(...)`."<< std::endl;
         exit(EXIT_FAILURE);
       }
-      if (   required_.has_value()) { handle . SetParameterName(name, required_.value()); }
+      if (   required_.has_value()) { handle.SetParameterName(name, required_.value()); }
       else {
         std::cerr << "Command " << name
                   << "has not been declared neither required nor optional. "
@@ -63,102 +71,21 @@ namespace nain4 {
 
     template<class... ArgTypes>
     messenger(ArgTypes&&... args) {
-      msg = std::make_unique<G4GenericMessenger>(std::forward<ArgTypes>(args)...);
+      msg = std::make_shared<G4GenericMessenger>(std::forward<ArgTypes>(args)...);
     }
 
     template<class VAR>
-    cmd_config add(G4String name, VAR& var) {
-      auto g4cmd = msg -> DeclareProperty(name, var);
-      return cmd_config{g4cmd, name};
+    cmd_config<VAR> add(G4String name, VAR& var) {
+      return {msg, name, var};
     }
 
   private:
-    std::unique_ptr<G4GenericMessenger> msg = nullptr;
+    shared_msg msg = nullptr;
   };
 
-
-#undef G4CMD
-#undef VAR_AND_SETTER
-
-
-  /*
-
-
-
-
-
-
-
-
-
-  using G4CMD = G4GenericMessenger::G4GenericMessenger::Command;
-
-  struct command {
-    G4CMD*   handle;
-    G4String name(){ std::cout << "NAME " << handle -> command -> GetCommandName() << std::endl;
-      return handle -> command -> GetCommandName(); }
-
-    command(G4CMD* handle) : handle(handle) {}
-
-#define VAR_AND_SETTER(NAME)                                 \
-    std::optional<G4String> NAME##_ = std::nullopt;          \
-    command& NAME(G4String x) { NAME##_ = x; return *this; }
-
-    VAR_AND_SETTER(  dimension)
-    VAR_AND_SETTER(       unit)
-    VAR_AND_SETTER(description)
-    VAR_AND_SETTER(      range)
-    VAR_AND_SETTER(    options)
-    VAR_AND_SETTER(defaults_to)
-
-    G4bool required_ = false;
-    command& required(){required_ =  true; return *this;}
-    command& optional(){required_ = false; return *this;}
-
-    void done() {
-      std::cout << "DONE HANDLE " << handle << std::endl;
-      if (  dimension_.has_value()) { handle -> SetUnitCategory(  dimension_.value()); }
-      if (       unit_.has_value()) { handle -> SetUnit        (       unit_.value()); }
-      if (description_.has_value()) { handle -> SetGuidance    (description_.value()); }
-      if (      range_.has_value()) { handle -> SetRange       (      range_.value()); }
-      if (    options_.has_value()) { handle -> SetCandidates  (    options_.value()); }
-      if (defaults_to_.has_value()) { handle -> SetDefaultValue(defaults_to_.value()); }
-
-      std::cout << "DONE HANDLE " << handle << std::endl;
-      handle -> SetParameterName(name(), required_);
-      std::cout << "DONE HANDLE " << handle << std::endl;
-    }
-  };
-
-
-// #define FORWARD(NEW_NAME, TYPE, OLD_NAME) command& NEW_NAME(TYPE v){handle.OLD_NAME(v); return *this;}
-//     FORWARD(  dimension, G4String, SetUnitCategory)
-//     FORWARD(       unit, G4String, SetUnit)
-//     FORWARD(description, G4String, SetGuidance)
-//     FORWARD(      range, G4String, SetRange)
-//     FORWARD(    options, G4String, SetCandidates)
-//     FORWARD( default_to, G4String, SetDefaultValue)
-
-//     #undef FORWARD
-
-//   private:
-//     G4CMD& handle;
-
-//     inline G4String get_name() { return handle.command->GetCommandName(); }
-//   };
-
-
-  struct messenger : public G4GenericMessenger {
-    using G4GenericMessenger::G4GenericMessenger;
-
-    template<class VAR>
-    command add(G4String name, VAR& variable) {
-      auto g4cmd = this -> DeclareProperty(name, variable, "");
-      std::cout << "G4CMD " << &g4cmd << std::endl;
-      return command{&g4cmd};
-    }
-  };
-  */
 }
+
+#undef shared_msg
+#undef VAR_AND_SETTER
 
 #endif // N4_MESSENGER_HH
